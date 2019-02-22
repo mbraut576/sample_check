d9abee3ef24df38f13cc135b30cd197877819e37
mbraut576 <47878246+mbraut576@users.noreply.github.com>
2019-02-22 12:27:44 +0530

    Create check.c

---
+++ check.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+int k=1;
+int read, get;
+read = k++ * k++ * k++;
+func();
+printf("Value = %d",read);
+
+}
+
+void func()
+{
+int divide_val,val;
+divide_val = val/0;
+printf("Divide Value = %d",divide_val);
+}
+
