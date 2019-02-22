2c63a054144818b19984272f1cc7a5a85bdd0ab9
mbraut576 <47878246+mbraut576@users.noreply.github.com>
2019-02-22 12:37:14 +0530

    Update check.c

--- check.c
+++ check.c
@@ -3,17 +3,25 @@
 int main()
 {
 int k=1;
-int read, get;
+int read;
+int get;
 read = k++ * k++ * k++;
 func();
+funct1();
 printf("Value = %d",read);
 
 }
 
 void func()
 {
-int divide_val,val;
+int divide_val;
+int val;
 divide_val = val/0;
 printf("Divide Value = %d",divide_val);
+}
+
+void func1()
+{
+  
 }
 
